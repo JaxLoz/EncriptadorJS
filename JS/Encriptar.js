8ba6b294b39cee90df83607ce005f78c0f697f35
Javier E Montes <jaxloz2002@gmail.com>
2023-07-04 00:19:01 -0500

    Cambios en las funciones encriptar y desencriptar para poder leer el texto de el textarea que resive los inputs

--- JS/Encriptar.js
+++ JS/Encriptar.js
@@ -1,29 +1,29 @@
 /*fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober! */
 /*felicidades por enfrentar este desafio y haberlo concluido con exito!*/
 
-let cad = "felicidades por enfrentar este desafio y haberlo concluido con exito!";
-let catEncrip = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";
-let cadenaAcentos = "denota una letra en cualquiera de los idiomas. También podemos usar";
+//let cad = "felicidades por enfrentar este desafio y haberlo concluido con exito!";
+//let catEncrip = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";
+//let cadenaAcentos = "denota una letra en cualquiera de los idiomas. También podemos usar";
+
+
 
 function EliminarAcentos (cadena){
     let cadenaSinAcento = cadena.replace(/é/ig, "e").replace(/í/ig, "i").replace(/á/ig, "a").replace(/ó/ig, "o").replace(/ú/ig, "u");
     return cadenaSinAcento;
 }
 
-function encriptar (cadena){
+function encriptar (){
+    let cadena = document.getElementById("area-encriptar").value;
     let cadenaLimpia = EliminarAcentos(cadena);
     let cadenaEncript = cadenaLimpia.replace(/e/ig, "enter").replace(/i/ig, "imes").replace(/a/ig, "ai").replace(/o/ig, "ober").replace(/u/ig, "ufat");
-    return cadenaEncript;
+    console.log(cadenaEncript);
 }
 
-function desencriptar (cadenaEncriptada){
+function desencriptar (){
+    let cadenaEncriptada = document.getElementById("area-encriptar").value;
     let cadenaDesencript = cadenaEncriptada.replace(/enter/ig, "e").replace(/imes/ig, "i").replace(/ai/ig, "a").replace(/ober/ig, "o").replace(/ufat/ig, "u");
-    return cadenaDesencript;
+    console.log(cadenaDesencript) ;
 }
 
-console.log(encriptar(cadenaAcentos))
-console.log(catEncrip);
 
-console.log("________________________________________________________");
-console.log(desencriptar(catEncrip));
 
