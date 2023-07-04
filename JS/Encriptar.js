7560c5f6ef163c1786f4a627e940fb80889c7dab
Javier E Montes <jaxloz2002@gmail.com>
2023-07-04 00:05:04 -0500

    Se agrego una funcion para quitar los acentos a el texto

--- JS/Encriptar.js
+++ JS/Encriptar.js
@@ -3,9 +3,16 @@
 
 let cad = "felicidades por enfrentar este desafio y haberlo concluido con exito!";
 let catEncrip = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";
+let cadenaAcentos = "denota una letra en cualquiera de los idiomas. También podemos usar";
+
+function EliminarAcentos (cadena){
+    let cadenaSinAcento = cadena.replace(/é/ig, "e").replace(/í/ig, "i").replace(/á/ig, "a").replace(/ó/ig, "o").replace(/ú/ig, "u");
+    return cadenaSinAcento;
+}
 
 function encriptar (cadena){
-    let cadenaEncript = cadena.replace(/e/ig, "enter").replace(/i/ig, "imes").replace(/a/ig, "ai").replace(/o/ig, "ober").replace(/u/ig, "ufat");
+    let cadenaLimpia = EliminarAcentos(cadena);
+    let cadenaEncript = cadenaLimpia.replace(/e/ig, "enter").replace(/i/ig, "imes").replace(/a/ig, "ai").replace(/o/ig, "ober").replace(/u/ig, "ufat");
     return cadenaEncript;
 }
 
@@ -14,7 +21,7 @@ function desencriptar (cadenaEncriptada){
     return cadenaDesencript;
 }
 
-console.log(encriptar(cad))
+console.log(encriptar(cadenaAcentos))
 console.log(catEncrip);
 
 console.log("________________________________________________________");
