748c8f980eeeef5a2f927702c912ccff170b3a4c
Javier E Montes <jaxloz2002@gmail.com>
2023-07-04 22:30:39 -0500

    Se agrego validacion de campos vacios para los textareas y se agrego la funcionalidad para que se elimie la vista de la imagen para

--- JS/Encriptar.js
+++ JS/Encriptar.js
@@ -5,24 +5,72 @@
 //let catEncrip = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";
 //let cadenaAcentos = "denota una letra en cualquiera de los idiomas. También podemos usar";
 
-
+let areaOut = document.getElementById("area-output");
+let areaInput = document.getElementById("area-encriptar");
+let divImagen = document.getElementById("img-content");
+let divTexOutput = document.getElementById("textAreaOutPut-content");
+divImagen.style.display = "block";
+divTexOutput.style.display = "block";
 
 function EliminarAcentos (cadena){
     let cadenaSinAcento = cadena.replace(/é/ig, "e").replace(/í/ig, "i").replace(/á/ig, "a").replace(/ó/ig, "o").replace(/ú/ig, "u");
     return cadenaSinAcento;
 }
 
+function validacionContenido (cadena){
+    if (cadena.match(/[\w\d]/) === null){
+        
+        return false;
+    }else{
+        return true;
+    }
+}
+
 function encriptar (){
-    let cadena = document.getElementById("area-encriptar").value;
-    let cadenaLimpia = EliminarAcentos(cadena);
-    let cadenaEncript = cadenaLimpia.replace(/e/ig, "enter").replace(/i/ig, "imes").replace(/a/ig, "ai").replace(/o/ig, "ober").replace(/u/ig, "ufat");
-    console.log(cadenaEncript);
+    
+    let cadena = areaInput.value;
+    let campoVacio = validacionContenido(cadena);
+    let estadoDivImage = divImagen.style.display;
+    let estadoDivTexOutput = divTexOutput.style.display;    
+
+    if (campoVacio === false){
+        alert("El campo de texto esta vacio o el texto que haz ingresado no es valido");
+    }else{
+
+        let cadenaLimpia = EliminarAcentos(cadena);
+        let cadenaEncript = cadenaLimpia.replace(/e/ig, "enter").replace(/i/ig, "imes").replace(/a/ig, "ai").replace(/o/ig, "ober").replace(/u/ig, "ufat");
+        areaOut.innerHTML = cadenaEncript;
+
+        if (estadoDivImage !== "none"){
+            divImagen.style.display = "none";
+            areaOut.innerHTML = cadenaEncript;
+        }else{
+            areaOut.innerHTML = cadenaEncript;
+        }
+    }
 }
 
 function desencriptar (){
-    let cadenaEncriptada = document.getElementById("area-encriptar").value;
-    let cadenaDesencript = cadenaEncriptada.replace(/enter/ig, "e").replace(/imes/ig, "i").replace(/ai/ig, "a").replace(/ober/ig, "o").replace(/ufat/ig, "u");
-    console.log(cadenaDesencript) ;
+    
+    let cadenaEncriptada = areaInput.value;
+    let campoVacio = validacionContenido(cadenaEncriptada);
+    let estadoDivImage = divImagen.style.display;
+    let estadoDivTexOutput = divTexOutput.style.display;
+
+    if (campoVacio === false){
+        alert("El campo de texto esta vacio o el texto que haz ingresado no es valido");
+    }else{
+
+        let cadenaDesencript = cadenaEncriptada.replace(/enter/ig, "e").replace(/imes/ig, "i").replace(/ai/ig, "a").replace(/ober/ig, "o").replace(/ufat/ig, "u");
+        if (estadoDivImage !== "none"){
+            divImagen.style.display = "none";
+            areaOut.innerHTML = cadenaDesencript;
+        }else{
+            areaOut.innerHTML = cadenaDesencript;
+        }
+     
+    }
+    
 }
 
 
